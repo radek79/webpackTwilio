f49bae48c8a97cb402774c49bc895275eb315312
Rod Patulski <rpatulski@wsoutput.onpeak.com>
2016-01-05 12:14:41 -0800

    composing 3 components using require

---
+++ commentbox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+var CommentList = require('./commentlist.jsx')
+var CommentForm = require('./commentform.jsx')
+
+ 
+module.exports = React.createClass({
+	render: function() {
+	return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+	}
+});
---
+++ commentlist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
