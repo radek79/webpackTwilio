30f1d133d2bd6def9c65f4045b53dbc229473341
Rod Patulski <rpatulski@wsoutput.onpeak.com>
2016-01-06 10:01:50 -0800

    submitting a form, data is passed from child component to parent component via callback. Server submission isn't working because there is no route set up

--- commentbox.jsx
+++ commentbox.jsx
@@ -19,6 +19,21 @@ module.exports = React.createClass({
 	    });
 	},
 
+	handleCommentSubmit: function(comment) {
+    	$.ajax({
+	      url: this.props.url,
+	      dataType: 'json',
+	      type: 'POST',
+	      data: comment,
+	      success: function(data) {
+	        this.setState({data: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.props.url, status, err.toString());
+	      }.bind(this)
+	    });
+  	},
+
 	getInitialState: function() {
 	    return {data: []};
 	},
@@ -34,7 +49,7 @@ module.exports = React.createClass({
 	        <CommentList data={this.state.data} />
 	        <br />
 	        <br />
-	        <CommentForm />
+	        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
 	      </div>
 	    );
 	}
