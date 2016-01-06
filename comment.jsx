6de588884943ac46297239412f8f37151bc60e63
Rod Patulski <rpatulski@wsoutput.onpeak.com>
2016-01-05 19:11:38 -0800

    added comment.jsx file

---
+++ comment.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
